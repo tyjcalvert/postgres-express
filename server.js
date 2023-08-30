d9c4d1b9b2e66cf95bf5937b376a7c3ed06da6be
Tyler Calvert <tylercalvert@Tylers-MacBook-Pro.local>
2023-08-30 14:15:09 -0500

    added cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const sequelize = require('./config/connection');
 const routes = require('./routes');
 
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use(routes);
 
