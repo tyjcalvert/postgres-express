dcf3cc854d1411b7bfb79fdd768ddf13ce764d7c
Tyler Calvert <tylercalvert@Tylers-MacBook-Pro.local>
2023-08-30 13:52:12 -0500

    updated to add user model

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening on port ' + PORT));
